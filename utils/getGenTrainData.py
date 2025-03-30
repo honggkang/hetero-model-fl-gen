bf1c75583ad44ddca1ea3773c11980dbd3b194a7
강홍구 <honggukang@HongKang-MacBook-Pro.local>
2025-03-31 01:24:44 +0900

    cifar10 add

--- utils/getGenTrainData.py
+++ utils/getGenTrainData.py
@@ -7,8 +7,10 @@ def generator_traindata(args):
         tf = transforms.Compose([transforms.Resize(args.img_size), transforms.ToTensor(),transforms.Normalize([0.5], [0.5])])
 
     if args.dataset == 'mnist':
-        train_data = datasets.MNIST(root='/home/hong/NeFL/.data/mnist', train=True, transform=tf, download=True)
+        train_data = datasets.MNIST(root='.data/mnist', train=True, transform=tf, download=True)
     elif args.dataset == 'fmnist':
-        train_data = datasets.FashionMNIST('/home/hong/NeFL/.data/fmnist', train=True, transform=tf, download=True)
+        train_data = datasets.FashionMNIST('.data/fmnist', train=True, transform=tf, download=True)
+    elif args.dataset == 'cifar10':
+        train_data = datasets.CIFAR10('.data/cifar10', train=True, transform=tf, download=True)        
     
     return train_data
